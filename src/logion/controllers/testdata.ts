d230b055610641f9b1ebab5acb126ed1fec5cdc2
Benoit Devos <benoit@logion.network>
2022-01-26 14:01:30 +0100

    feat: add postal address to API.
    logion-network/logion-internal#328

--- src/logion/controllers/testdata.ts
+++ src/logion/controllers/testdata.ts
@@ -11,8 +11,15 @@ export const LEGAL_OFFICERS: LegalOfficerView[] = [
             email: "patrick@logion.network",
             phoneNumber: "+32 498 237 107",
         },
-        details: "MODERO\nHuissier de Justice Etterbeek\nRue Beckers 17\n1040 Etterbeek\nBelgique",
-        node: "http://localhost:8080"
+        postalAddress: {
+            company: "MODERO",
+            line1: "Huissier de Justice Etterbeek",
+            line2: "Rue Beckers 17",
+            postalCode: "1040",
+            city: "Etterbeek",
+            country: "Belgique"
+        },
+        node: "http://localhost:8080",
     },
     {
         address: "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty",
@@ -22,8 +29,15 @@ export const LEGAL_OFFICERS: LegalOfficerView[] = [
             email: "g.grain@adrastee-lyon.fr",
             phoneNumber: "+33 4 78 52 87 56",
         },
-        details: "SELARL ADRASTEE\nGare des Brotteaux\n14, place Jules Ferry\n69006 LYON\nFrance",
-        node: "http://localhost:8081"
+        postalAddress: {
+            company: "SELARL ADRASTEE",
+            line1: "Gare des Brotteaux",
+            line2: "14, place Jules Ferry",
+            postalCode: "69006",
+            city: "Lyon",
+            country: "France"
+        },
+        node: "http://localhost:8081",
     },
     {
         address: "5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y",
@@ -33,7 +47,48 @@ export const LEGAL_OFFICERS: LegalOfficerView[] = [
             email: "alain.barland@auxilia-conseils.com",
             phoneNumber: "+33 2 48 67 50 50",
         },
-        details: "AUXILIA CONSEILS 18\nHuissiers de Justice associés\n7 rue Jean Francois Champollion Parc Comitec\n18000 Bourges\nFrance",
+        postalAddress: {
+            company: "AUXILIA CONSEILS 18",
+            line1: "Huissiers de Justice associés",
+            line2: "7 rue Jean Francois Champollion Parc Comitec",
+            postalCode: "18000",
+            city: "Bourges",
+            country: "France"
+        },
+        node: "http://localhost:8082",
+    },
+    {
+        address: "5FmqTpGanDBVHedXf42fiuWD8d2iBa2Ve8EfG13juifnpgat",
+        userIdentity: {
+            firstName: "Eline",
+            lastName: "Duysens",
+            email: "e.duysens@chartierpartners.be",
+            phoneNumber: "+32 82 22 50 50"
+
+        },
+        postalAddress: {
+            line1: "Rue du Calvaire, 1/A",
+            postalCode: "5620",
+            city: "FLORENNES",
+            country: "Belgique"
+        },
+        additionalDetails: "CHARTIER & PARTNERS\nSiège administratif :\nzoning de la voie cuivrée, 34\n5503 Sorinnes (Dinant)\nBelgique\nEmail: info@chartierpartners.be",
         node: "http://localhost:8082"
+    },
+    {
+        address: "5GYirZEq8byGJePM9FM3JQG8Zwc5B6AcNpqgbrFvGRw2VQKE",
+        userIdentity: {
+            firstName: "Sylvian",
+            lastName: "Dorol",
+            email: "s.dorol@venezia-huissiers.com",
+            phoneNumber: "+ 33 1 46 24 15 21",
+        },
+        postalAddress: {
+            company: "Venezia & Associés",
+            line1: "130 avenue Charles de Gaulle",
+            postalCode: "92200",
+            city: "Neuilly sur seine",
+            country: "France"
+        }
     }
 ]
--- test/unit/controllers/legalofficer.controller.spec.ts
+++ test/unit/controllers/legalofficer.controller.spec.ts
@@ -1,19 +1,19 @@
 import { setupApp } from "../../helpers/testapp";
 import { LegalOfficerController } from "../../../src/logion/controllers/legalofficer.controller";
-import request, { Response } from "supertest";
+import request from "supertest";
 
 describe("LegalOfficerController", () => {
 
     it("should fetch all legal officers", async () => {
 
-        const app = setupApp(LegalOfficerController, container => {
+        const app = setupApp(LegalOfficerController, () => {
         })
         await request(app)
             .get("/api/legal-officer")
             .expect(200)
             .expect('Content-Type', /application\/json/)
             .then(response => {
-                expect(response.body.legalOfficers.length).toBe(3)
+                expect(response.body.legalOfficers.length).toBe(5)
             });
     });
 
@@ -26,11 +26,18 @@ describe("LegalOfficerController", () => {
             .expect('Content-Type', /application\/json/)
             .then(response => {
                 expect(response.body.address).toBe("5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY")
-                expect(response.body.userIdentity.firstName).toBe("Patrick")
-                expect(response.body.userIdentity.lastName).toBe("Gielen")
-                expect(response.body.userIdentity.email).toBe("patrick@logion.network")
-                expect(response.body.userIdentity.phoneNumber).toBe("+32 498 237 107")
-                expect(response.body.details).toBe("MODERO\nHuissier de Justice Etterbeek\nRue Beckers 17\n1040 Etterbeek\nBelgique")
+                const userIdentity = response.body.userIdentity;
+                expect(userIdentity.firstName).toBe("Patrick")
+                expect(userIdentity.lastName).toBe("Gielen")
+                expect(userIdentity.email).toBe("patrick@logion.network")
+                expect(userIdentity.phoneNumber).toBe("+32 498 237 107")
+                let postalAddress = response.body.postalAddress;
+                expect(postalAddress.company).toBe("MODERO")
+                expect(postalAddress.line1).toBe("Huissier de Justice Etterbeek")
+                expect(postalAddress.line2).toBe("Rue Beckers 17")
+                expect(postalAddress.postalCode).toBe("1040")
+                expect(postalAddress.city).toBe("Etterbeek")
+                expect(postalAddress.country).toBe("Belgique")
                 expect(response.body.node).toBe("http://localhost:8080")
             });
     })
