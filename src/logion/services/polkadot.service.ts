4ad8852cf9509e931934551e3f4e0431d0e59c26
Gérard Dethier <gerard@logion.network>
2022-03-23 15:33:06 +0100

    feat: use logion-api.

--- src/logion/services/polkadot.service.ts
+++ src/logion/services/polkadot.service.ts
@@ -1,9 +1,7 @@
 import { injectable } from 'inversify';
-import '../lib/polkadot/interfaces/augment-api';
-import '../lib/polkadot/interfaces/augment-types';
-import { ApiPromise, WsProvider } from '@polkadot/api';
-import * as definitions from '../lib/polkadot/interfaces/definitions';
+import { ApiPromise } from '@polkadot/api';
 import { Log } from "../util/Log";
+import { buildApi } from "logion-api/dist/Connection";
 
 const { logger } = Log;
 
@@ -12,22 +10,10 @@ export class PolkadotService {
 
     async readyApi(): Promise<ApiPromise> {
         if (this._api === null) {
-            this._api = await this._createApi();
+            this._api = await buildApi(process.env.WS_PROVIDER_URL || 'ws://localhost:9944');
         }
         return this._api;
     }
 
     private _api: ApiPromise | null = null;
-
-    private async _createApi(): Promise<ApiPromise> {
-        const wsProviderUrl = process.env.WS_PROVIDER_URL || 'ws://localhost:9944';
-        logger.info("Connecting to edge node(s) %s", wsProviderUrl);
-        const sockets = wsProviderUrl.split(",")
-        const wsProvider = new WsProvider(sockets);
-        const types = Object.values(definitions).reduce((res, { types }): object => ({ ...res, ...types }), {});
-        return await ApiPromise.create({
-            provider: wsProvider,
-            types
-        });
-    }
 }
